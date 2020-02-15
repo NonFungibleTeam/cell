09784855cd7d87bfa1221d3f45afbb9746448ef3
Skyler Fly-Wilson <skyfly200@gmail.com>
2020-02-15 13:02:10 -0700

    setup drizzle

--- dapp/src/main.ts
+++ dapp/src/main.ts
@@ -5,6 +5,12 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 
+import drizzleVuePlugin from "@drizzle/vue-plugin";
+import drizzleOptions from "./drizzleOptions";
+
+// Register the drizzleVuePlugin
+Vue.use(drizzleVuePlugin, { store, drizzleOptions });
+
 Vue.config.productionTip = false;
 
 new Vue({
