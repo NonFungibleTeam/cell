028d05a2112d42c97df52737cfee8ae6b5f3ce59
Skyler Fly-Wilson <skyfly200@gmail.com>
2020-02-25 01:08:46 -0700

    add cell svg and core info to CellPage

--- dapp/src/views/CellPage.vue
+++ dapp/src/views/CellPage.vue
@@ -1,9 +1,14 @@
 <template lang="pug">
-  v-container.mint
+  .cell-page
     Nav
-    v-row(cols=6).landing
-        v-col(align="center").content
-          h1 # {{ $route.params.id }}
+    v-card
+      v-container
+        v-row
+          v-col(align="center").cell-graphic
+            Cell(:id="$route.params.id" :mass="cell.mass" :features="cell.features")
+          v-col.cell-info
+            h1 # {{ $route.params.id }}
+            h2 Level: {{ level }}
 
     
 </template>
@@ -15,7 +20,48 @@ import Nav from "@/components/Nav.vue";
 
 export default Vue.extend({
   name: "CellPage",
-  components: { Nav, Cell }
+  components: { Nav, Cell },
+  computed: {
+    level() {
+      return Math.floor(Math.log2(this.cell.mass)) - 2;
+    }
+  },
+  data: () => ({
+    cell: {
+      mass: 541,
+      features: {
+        body: {
+          rounded: false,
+          waves: [2, 1, 1, 0],
+          color: "#efcc35",
+          gradient: ["#ccddcc", "#773311", "#337744"]
+        },
+        nucleus: {
+          color: "#f56",
+          size: 60
+        },
+        endo: {
+          color: "#00f"
+        },
+        mitocondria: {
+          color: "#f33",
+          count: 6
+        },
+        chloroplasts: {
+          color: "#3f5",
+          count: 4
+        },
+        lisosomes: {
+          color: "#ff0",
+          count: 1
+        },
+        ribosomes: {
+          color: "#66f",
+          count: 4
+        }
+      }
+    }
+  })
 });
 </script>
 
