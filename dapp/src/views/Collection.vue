ac95920476f6e5207084b2bbeb382fa8c06de0fa
Skyler Fly-Wilson <skyfly200@gmail.com>
2020-02-27 05:43:18 -0700

    simple merge UI mockup

--- dapp/src/views/Collection.vue
+++ dapp/src/views/Collection.vue
@@ -11,12 +11,12 @@
               v-spacer 
               Level(:mass="cell.mass")
             v-card-text.cell-wrapper
-              Cell(:id="i" :mass="cell.mass" :features="cell.features")
+              Cell(:id="'merge' + i" :mass="cell.mass" :features="cell.features")
             v-divider
             v-card-actions
               v-spacer
               v-btn(:to="'/cell/' + i") View
-              v-btn(color="primary") Merge
+              v-btn(color="primary" @click="sheet = !sheet") Merge
               v-btn(color="primary") Divide
         v-col(v-if="!cells.length").get-started
           v-card(align="center").get-started-card
@@ -25,6 +25,30 @@
             span &nbsp;or&nbsp;
             v-btn(outlined color="secondary") Buy
             p one to get started
+    v-bottom-sheet(v-model="sheet" inset)
+      v-sheet(align="center" height="430px")
+        v-container
+          v-row
+            v-col
+              .stats-bar
+                span {{ "#" + merge[0] }}
+                .mass
+                  span {{ cells[merge[0]].mass }}
+                  v-icon(large) mdi-atom
+                Level(:mass="cells[merge[0]].mass")
+              Cell(:id="merge[0]" :mass="cells[merge[0]].mass" :features="cells[merge[0]].features")
+            v-divider(vertical)
+            v-col
+              .stats-bar
+                span {{ "#" + merge[1] }}
+                .mass
+                  span {{ cells[merge[1]].mass }}
+                  v-icon(large) mdi-atom
+                Level(:mass="cells[merge[1]].mass")
+              Cell(:id="merge[1]" :mass="cells[merge[1]].mass" :features="cells[merge[1]].features")
+        .merge-btns
+          v-btn(class="mt-6" text color="success" @click="sheet = !sheet") Merge
+          v-btn(class="mt-6" text color="error" @click="sheet = !sheet") Cancel
 </template>
 
 <script>
@@ -35,10 +59,12 @@ import Level from "@/components/Level.vue";
 import cellUtils from "@/mixins/cellUtils";
 
 export default {
-  name: "Home",
+  name: "Collection",
   mixins: [cellUtils],
   components: { Nav, Cell, Level },
   data: () => ({
+    sheet: true,
+    merge: [1, 3],
     cells: [
       {
         mass: 15,
@@ -197,4 +223,13 @@ export default {
   display: flex
   flex-direction: column
   padding: 2rem 
+.container
+  padding: 0
+.stats-bar
+  display: flex
+  justify-content: space-between
+  padding: 0 1rem
+.merge-btns
+  display: flex
+  justify-content: space-around
 </style>
