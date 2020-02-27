052372a38784bd3041ad6067ff03bf0651e514ab
Skyler Fly-Wilson <skyfly200@gmail.com>
2020-02-26 21:58:04 -0700

    mobile account view

--- dapp/src/components/Nav.vue
+++ dapp/src/components/Nav.vue
@@ -2,7 +2,13 @@
   .nav
     v-navigation-drawer(app right temporary v-model="drawer")
       v-list(nav dense)
-        .link(v-for="l,i in links" :key="i")
+        template(v-if="!isDrizzleInitialized")
+          v-list-item(two-line)
+            Gravatar.gravatar.gravatar-mobile(:size="50" :email="activeAccount !== '' ? activeAccount : defaultAccount")
+            v-list-item-content 
+              span.address {{ formatAccount(activeAccount !== '' ? activeAccount : defaultAccount) }}
+          v-divider
+        .mobile-link(v-for="l,i in links" :key="i")
           v-list-item(v-if="l.type === 'page'" :to="l.path" link)
             v-list-item-icon
               v-icon {{ l.icon }}
@@ -21,7 +27,7 @@
           span.mr-2 {{ l.text }}
         v-btn(v-else-if="l.type === 'link'" :href="l.path" target="_blank" icon)
           v-icon {{ l.icon }}
-      Gravatar.gravatar(v-if="isDrizzleInitialized" size=40 :email="activeAccount")
+      Gravatar.gravatar(v-if="isDrizzleInitialized" :size="40" :email="activeAccount")
       v-btn(@click="drawer = !drawer" text).mobile-menu-btn
         v-icon mdi-menu
         
@@ -36,8 +42,14 @@ Vue.component("v-gravatar", Gravatar);
 
 export default {
   components: { Gravatar },
+  methods: {
+    formatAccount(account) {
+      return "0x" + account.slice(0, 4) + "...." + account.slice(-5, -1);
+    }
+  },
   data: () => ({
     drawer: false,
+    defaultAccount: "268b87E4F6B7e7BEB58e3128138D4F6b768E1b17",
     links: [
       {
         type: "page",
@@ -81,11 +93,13 @@ export default {
 .gravatar
   margin-top: -7px
   border-radius: 50%
+.gravatar-mobile
+  margin-right: 10px
 .mobile-menu-btn
     display: none !important
 @media(max-width: 700px)
   .mobile-menu-btn
-    display: block
+    display: block !important
   .link
-    display: none
+    display: none !important
 </style>
