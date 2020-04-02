a92b96b26f144c5d19b22192c9f947c98c1ee2db
Skyler Fly-Wilson <skyfly200@gmail.com>
2020-04-01 20:55:02 -0600

    reorder functions in cell render for faster edits

--- dapp/src/mixins/cellRender.ts
+++ dapp/src/mixins/cellRender.ts
@@ -20,70 +20,6 @@ const tao = 2 * Math.PI;
 
 const cellRender: any = {
   methods: {
-    intToColor(intnumber: number) {
-      // bit shift color channel components
-      const red = (intnumber & 0x0000ff) << 16;
-      const green = intnumber & 0x00ff00;
-      const blue = (intnumber & 0xff0000) >>> 16;
-
-      // mask out each color and reverse the order
-      intnumber = red | green | blue;
-
-      // convert number to a hexstring, zero fill on left & add a #
-      const HTMLcolor = intnumber.toString(16);
-      const template = "#000000";
-      return template.substring(0, 7 - HTMLcolor.length) + HTMLcolor;
-    },
-
-    getRandomInt(max: number) {
-      return Math.floor(Math.random() * Math.floor(max));
-    },
-
-    contrastingColor(hex: string) {
-      const [R, G, B] = [0, 1, 2].map(i =>
-        parseInt(hex.substring(i * 2 + 1, i * 2 + 3), 16)
-      );
-      const cBrightness = (R * 299 + G * 587 + B * 114) / 1000;
-      const threshold = 100; /* about half of 256. Lower threshold equals more dark text on dark background  */
-      return cBrightness > threshold ? "#000000" : "#ffffff";
-    },
-
-    mergeWaves(waves: Array<Array<number>>, bitDepthMax: number) {
-      const waveLengths = waves.map(w => w.length);
-      const lcm = this.lcmNumbers(waveLengths); // least common multiple of the length of the wave arrays
-      const compoundWave: Array<number> = [];
-      for (let i = 0; i < lcm; i++) {
-        compoundWave[i] = 0;
-        for (const wave of waves) {
-          const j = Math.floor(i / (lcm / wave.length));
-          compoundWave[i] += wave[j] / (bitDepthMax - 1);
-        }
-      }
-      return compoundWave;
-    },
-
-    lcmNumbers(inputArray: Array<number>): number {
-      if (toString.call(inputArray) !== "[object Array]") return -1;
-      let r1 = 0,
-        r2 = 0;
-      const l = inputArray.length;
-      for (let i = 0; i < l; i++) {
-        r1 = inputArray[i] % inputArray[i + 1];
-        if (r1 === 0) {
-          inputArray[i + 1] =
-            (inputArray[i] * inputArray[i + 1]) / inputArray[i + 1];
-        } else {
-          r2 = inputArray[i + 1] % r1;
-          if (r2 === 0) {
-            inputArray[i + 1] = (inputArray[i] * inputArray[i + 1]) / r1;
-          } else {
-            inputArray[i + 1] = (inputArray[i] * inputArray[i + 1]) / r2;
-          }
-        }
-      }
-      return inputArray[l - 1];
-    },
-
     drawCell(
       data: any,
       waveform: Array<number>,
@@ -96,7 +32,7 @@ const cellRender: any = {
       const draw = SVG()
         .addTo(target)
         .size(size + margin * 2, size + margin * 2);
-      
+
       // seed rng with wave shape
       const seed = data.wallWave;
       const rand = randomSeed.create(seed);
@@ -137,7 +73,7 @@ const cellRender: any = {
           category: data.featureCategories[i],
           family: data.featureFamilies[i],
           count: data.featureCounts[i] as number,
-          color: this.intToColor(data.featureColors[i]),
+          color: this.intToColor(data.featureColors[i])
         };
         //feature.fill = c === "2" ? mitoPattern(color) : color
         if (feature.category === "0") {
@@ -171,65 +107,6 @@ const cellRender: any = {
 
     getFeatureFamily(i: number): string {
       return families[i].title;
-    },
-
-    countList(list: Array<any>) {
-      // count and sort by family with most features
-      return list.reduce((count: any, f: string) => {
-        count[parseInt(f)] ? count[parseInt(f)]++ : (count[parseInt(f)] = 1);
-        return count;
-      }, {});
-    },
-
-    sortObject(counts: any) {
-      return Object.keys(counts).sort((a, b) => counts[b] - counts[a]);
-    },
-
-    drawBody(
-      draw: any,
-      waveform: Array<number>,
-      count: number,
-      size: number,
-      data: any,
-      margin: number
-    ) {
-      // plot shape from wave
-      const shape = this.plotShape(size, waveform, count);
-
-      const [[minX, maxX], [minY, maxY]] = this.wallRange(shape);
-      const w = maxX - minX;
-      const h = maxY - minY;
-
-      // calculate gradient
-      const counts = this.countList(data.featureFamilies);
-      const sorted = this.sortObject(counts);
-      const gradient = draw.gradient("radial", function(add: any) {
-        let a = 0;
-        for (const i of sorted) {
-          a += counts[i];
-          add.stop({
-            offset: a / 10 + 0.25,
-            color: families[parseInt(i)].color,
-            opacity: cytoplasmOpacity
-          });
-        }
-      });
-
-      // body - draw cell wall and fill
-      const body = data.wallRound
-        ? draw.path(svgPath(shape))
-        : draw.polygon(shape);
-      body
-        .move(margin + (size - w) / 2, margin + (size - h) / 2)
-        .fill(gradient)
-        .stroke({
-          width: 3,
-          color: this.intToColor(data.wallColor),
-          linecap: "round",
-          linejoin: "round"
-        });
-
-      return shape;
     },
 
     drawNucleus(
@@ -297,6 +174,65 @@ const cellRender: any = {
       }
     },
 
+    drawBody(
+      draw: any,
+      waveform: Array<number>,
+      count: number,
+      size: number,
+      data: any,
+      margin: number
+    ) {
+      // plot shape from wave
+      const shape = this.plotShape(size, waveform, count);
+
+      const [[minX, maxX], [minY, maxY]] = this.wallRange(shape);
+      const w = maxX - minX;
+      const h = maxY - minY;
+
+      // calculate gradient
+      const counts = this.countList(data.featureFamilies);
+      const sorted = this.sortObject(counts);
+      const gradient = draw.gradient("radial", function(add: any) {
+        let a = 0;
+        for (const i of sorted) {
+          a += counts[i];
+          add.stop({
+            offset: a / 10 + 0.25,
+            color: families[parseInt(i)].color,
+            opacity: cytoplasmOpacity
+          });
+        }
+      });
+
+      // body - draw cell wall and fill
+      const body = data.wallRound
+        ? draw.path(svgPath(shape))
+        : draw.polygon(shape);
+      body
+        .move(margin + (size - w) / 2, margin + (size - h) / 2)
+        .fill(gradient)
+        .stroke({
+          width: 3,
+          color: this.intToColor(data.wallColor),
+          linecap: "round",
+          linejoin: "round"
+        });
+
+      return shape;
+    },
+
+    countList(list: Array<any>) {
+      // count and sort by family with most features
+      return list.reduce((count: any, f: string) => {
+        count[parseInt(f)] ? count[parseInt(f)]++ : (count[parseInt(f)] = 1);
+        return count;
+      }, {});
+    },
+
+    sortObject(counts: any) {
+      return Object.keys(counts).sort((a, b) => counts[b] - counts[a]);
+    },
+
     wallRange(shape: Array<Array<number>>) {
       return shape.reduce(
         function(result: Array<Array<number>>, cords: Array<number>) {
@@ -319,7 +255,7 @@ const cellRender: any = {
     plotShape(size: number, wave: Array<number>, repeat: number) {
       const radius = size / 2;
       const segments = repeat * wave.length;
-      const points = [];  
+      const points = [];
       for (let i = 0; i <= segments; i++)
         points[i] = this.radialWavePlotter(i, radius, wave, segments);
       return points;
@@ -336,6 +272,70 @@ const cellRender: any = {
       const x = Math.round(Math.sin((tao * i) / segments) * scale);
       const y = Math.round(Math.cos((tao * i) / segments) * scale * -1);
       return [x, y];
+    },
+
+    mergeWaves(waves: Array<Array<number>>, bitDepthMax: number) {
+      const waveLengths = waves.map(w => w.length);
+      const lcm = this.lcmNumbers(waveLengths); // least common multiple of the length of the wave arrays
+      const compoundWave: Array<number> = [];
+      for (let i = 0; i < lcm; i++) {
+        compoundWave[i] = 0;
+        for (const wave of waves) {
+          const j = Math.floor(i / (lcm / wave.length));
+          compoundWave[i] += wave[j] / (bitDepthMax - 1);
+        }
+      }
+      return compoundWave;
+    },
+
+    lcmNumbers(inputArray: Array<number>): number {
+      if (toString.call(inputArray) !== "[object Array]") return -1;
+      let r1 = 0,
+        r2 = 0;
+      const l = inputArray.length;
+      for (let i = 0; i < l; i++) {
+        r1 = inputArray[i] % inputArray[i + 1];
+        if (r1 === 0) {
+          inputArray[i + 1] =
+            (inputArray[i] * inputArray[i + 1]) / inputArray[i + 1];
+        } else {
+          r2 = inputArray[i + 1] % r1;
+          if (r2 === 0) {
+            inputArray[i + 1] = (inputArray[i] * inputArray[i + 1]) / r1;
+          } else {
+            inputArray[i + 1] = (inputArray[i] * inputArray[i + 1]) / r2;
+          }
+        }
+      }
+      return inputArray[l - 1];
+    },
+
+    intToColor(intnumber: number) {
+      // bit shift color channel components
+      const red = (intnumber & 0x0000ff) << 16;
+      const green = intnumber & 0x00ff00;
+      const blue = (intnumber & 0xff0000) >>> 16;
+
+      // mask out each color and reverse the order
+      intnumber = red | green | blue;
+
+      // convert number to a hexstring, zero fill on left & add a #
+      const HTMLcolor = intnumber.toString(16);
+      const template = "#000000";
+      return template.substring(0, 7 - HTMLcolor.length) + HTMLcolor;
+    },
+
+    getRandomInt(max: number) {
+      return Math.floor(Math.random() * Math.floor(max));
+    },
+
+    contrastingColor(hex: string) {
+      const [R, G, B] = [0, 1, 2].map(i =>
+        parseInt(hex.substring(i * 2 + 1, i * 2 + 3), 16)
+      );
+      const cBrightness = (R * 299 + G * 587 + B * 114) / 1000;
+      const threshold = 100; /* about half of 256. Lower threshold equals more dark text on dark background  */
+      return cBrightness > threshold ? "#000000" : "#ffffff";
     },
   }
 };
